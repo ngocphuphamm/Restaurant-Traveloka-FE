07a4ce56e92c79bf9b7de81476ab297c9e855f3d
ngocphuphamm <ngocphupham682001@gmail.com>
2022-05-20 08:52:16 +0700

    edit

--- src/redux/rootReducer.jsx
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import cartReducer from "../redux/Cart/cart-reducer";
-
-const rootReducer = combineReducers({
-  cart : cartReducer ,
-});
-
-export default rootReducer;
